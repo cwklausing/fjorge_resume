324738c618b54d587b0593e3168a3d110f27a080
cwklausing <cwklausing@gmail.com>
2016-06-15 10:56:45 -0500

    fjorge resume initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function lightBoxOn(){
+    var lightboxContainer = document.getElementById("lightBoxContainer");
+
+    lightboxContainer.style.display = "block";
+
+}
+
+function lightBoxOff(){
+    var lightboxContainer = document.getElementById("lightBoxContainer");
+
+    lightboxContainer.style.display = "none";
+}
